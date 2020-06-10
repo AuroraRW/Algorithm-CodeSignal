5a852bd10d196a9c8eab76dacb34ce551144dafc
AuroraRW <wangrong19810806@gmail.com>
2020-06-09 23:03:29 -0400

    update Intro 01-04

---
+++ Intro/N01-05.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSignalCSharp
+{
+    class N01_05
+    {
+        int add(int param1, int param2)
+        {
+            return (param1 + param2);
+        }
+        int centuryFromYear(int year)
+        {
+            return ((year - 1) / 100 + 1);
+        }
+        int centuryFromYear1(int year)
+        {
+            return Convert.ToInt32(Math.Ceiling(Convert.ToDecimal(year) / 100));
+        }
+        bool checkPalindrome(string inputString)
+        {
+            string result = new string(inputString.Reverse().ToArray());
+            return (inputString == result);
+        }
+        bool checkPalindrome1(string myString)
+        {
+            return myString.SequenceEqual(myString.Reverse());
+        }
+
+        int adjacentElementsProduct(int[] inputArray)
+        {
+            int m = int.MinValue;
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                m = Math.Max(m, inputArray[i] * inputArray[i + 1]);
+            }
+            return m;
+        }
+    }
+}
